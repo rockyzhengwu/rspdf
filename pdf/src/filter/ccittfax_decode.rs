265c0a30b6dad8512e3d860cb1c3572abc19dc77
zhengwu <zhengwu@midday.me>
2024-05-16 07:57:05 +0800

    feat: fax decode

--- pdf/src/filter/ccittfax_decode.rs
+++ pdf/src/filter/ccittfax_decode.rs
@@ -1,5 +1,70 @@
-use crate::errors::PDFResult;
+use crate::errors::{PDFError, PDFResult};
 use crate::object::PDFObject;
+use fax::{
+    decoder::{decode_g4, pels},
+    Color,
+};
+
+pub struct BitReader {
+    data: Vec<u8>,
+    current_index: usize,
+}
+
+impl BitReader {
+    pub fn new(data: Vec<u8>) -> Self {
+        BitReader {
+            data,
+            current_index: 0,
+        }
+    }
+
+    pub fn peek_u16(&self, bits: usize) -> PDFResult<u16> {
+        if bits > 16 {
+            return Err(PDFError::Filter(
+                "ccitdecoder reader only peek 16 bits once".to_string(),
+            ));
+        }
+
+        if (self.current_index + bits) > self.data.len() * 8 {
+            return Err(PDFError::Filter("ccitdecoder reader eof".to_string()));
+        }
+        let n = self.current_index / 8;
+        let delta = self.current_index % 8;
+        let current_value = self.data.get(n).unwrap().to_owned() as u16;
+        let current_remain = 8 - delta;
+
+        if bits < current_remain {
+            let v = current_value & ((1 << current_remain) - 1);
+            Ok(v >> (current_remain - bits))
+        } else {
+            let first = current_value & ((1 << current_remain) - 1);
+            let mut result = first << (bits - current_remain);
+            let mut remain = bits - current_remain;
+            let mut nn = n + 1;
+
+            while remain > 0 {
+                let nv = self.data.get(nn).unwrap().to_owned() as u16;
+                if remain >= 8 {
+                    result += nv << (remain - 8);
+                    remain -= 8;
+                } else {
+                    result += nv >> (remain);
+                    break;
+                }
+                nn += 1;
+            }
+            Ok(result)
+        }
+    }
+
+    pub fn move_bits(&mut self, bits: usize) {
+        self.current_index += bits;
+    }
+}
+
+struct FaxDecoder {
+    reader: BitReader,
+}
 
 pub fn fax_decode(buf: &[u8], param: Option<&PDFObject>) -> PDFResult<Vec<u8>> {
     let mut k = 0;
@@ -33,8 +98,19 @@ pub fn fax_decode(buf: &[u8], param: Option<&PDFObject>) -> PDFResult<Vec<u8>> {
             damaged_rows_before_error = d.as_u32()?;
         }
     }
+    // TODO fix this just use fax right now
+    let mut res = Vec::with_capacity((columns * rows) as usize);
+    decode_g4(
+        buf.iter().cloned(),
+        columns as u16,
+        Some(rows as u16),
+        |line| {
+            res.extend(pels(line, columns as u16).map(|c| match c {
+                Color::Black => 0,
+                Color::White => 255,
+            }));
+        },
+    );
 
-    println!("{:?},{:?},{:?}", k, columns, rows);
-
-    unimplemented!()
+    Ok(res)
 }
